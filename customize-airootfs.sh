c54c5780bf5e04a0444cc744919a6928bf63d76d
sulincix <55052890+sulincix@users.noreply.github.com>
2022-01-28 12:01:00 +0000

    Update customize-airootfs.sh

--- customize-airootfs.sh
+++ customize-airootfs.sh
@@ -8,8 +8,11 @@ cd /tmp/
 wget https://github.com/PuffOS/base-files/releases/download/v1/base-files_9999-noupdate_amd64.deb
 wget https://github.com/PuffOS/desktop-base/releases/download/1/desktop-base_9999-noupdate_all.deb
 wget https://github.com/PuffOS/17g-installer/releases/download/4.3/17g-installer_1.0_all.deb
+wget https://github.com/PuffOS/disable-secondary-gpu/releases/download/current/disable-secondary-gpu_0.1.0_all.deb
 apt install ./*.deb -yq --allow-downgrades
 rm -f *.deb
 # Purge shitty display manager.
 apt purge gdm3 -yq
 apt autoremove -yq
+rm -f /bin/sh
+ln -s bash /bin/sh
