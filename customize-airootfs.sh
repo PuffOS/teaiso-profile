f7aa05b39691742819c341d9b0f20c19a3b7ccbf
sulincix <55052890+sulincix@users.noreply.github.com>
2022-02-26 12:56:46 +0000

    Update customize-airootfs.sh

--- customize-airootfs.sh
+++ customize-airootfs.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
-# Fix invoke-rc.d issue
+
+# fake dummy systemctl command
 touch /usr/bin/systemctl
 chmod +x /usr/bin/systemctl
+
+#### Remove useless files after package installation by default
+cat > /etc/apt/apt.conf.d/02cleanup << EOF
+DPkg::Post-Invoke {"rm -rf /usr/share/man || true";};
+DPkg::Post-Invoke {"rm -rf /usr/share/help || true";};
+DPkg::Post-Invoke {"rm -rf /usr/share/doc || true";};
+DPkg::Post-Invoke {"rm -rf /usr/share/info || true";};
+EOF
+
 # install packages
 cd /tmp/
 wget https://github.com/PuffOS/base-files/releases/download/v1/base-files_9999-noupdate_amd64.deb
@@ -13,6 +23,10 @@ wget https://github.com/PuffOS/deb-packages/raw/main/gnome-session-common_9999_a
 wget https://github.com/PuffOS/deb-packages/raw/main/gnome-session_9999_all.deb
 apt install ./*.deb -yq --allow-downgrades
 rm -f *.deb
+# remove broken sessions (FIXME)
+rm -f /usr/share/wayland-sessions/*
+rm -f /usr/share/xsessions/lightdm-xsession.desktop
+
 # Purge shitty display manager.
 apt purge gdm3 -yq
 apt autoremove -yq
@@ -23,12 +37,4 @@ ln -s bash /bin/sh
 cat > /etc/apt/apt.conf.d/01norecommend << EOF
 APT::Install-Recommends "0";
 APT::Install-Suggests "0";
-EOF
-
-#### Remove useless files after package installation by default
-cat > /etc/apt/apt.conf.d/02cleanup << EOF
-DPkg::Post-Invoke {"rm -rf /usr/share/man || true";};
-DPkg::Post-Invoke {"rm -rf /usr/share/help || true";};
-DPkg::Post-Invoke {"rm -rf /usr/share/doc || true";};
-DPkg::Post-Invoke {"rm -rf /usr/share/info || true";};
 EOF
