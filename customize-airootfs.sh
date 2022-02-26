761c745438a8e876949164852d52d33a4ead077a
sulincix <55052890+sulincix@users.noreply.github.com>
2022-02-26 11:50:20 +0000

    Update customize-airootfs.sh

--- customize-airootfs.sh
+++ customize-airootfs.sh
@@ -18,3 +18,17 @@ apt purge gdm3 -yq
 apt autoremove -yq
 rm -f /bin/sh
 ln -s bash /bin/sh
+
+#### Disable recommends by default
+cat > /etc/apt/apt.conf.d/01norecommend << EOF
+APT::Install-Recommends "0";
+APT::Install-Suggests "0";
+EOF
+
+#### Remove useless files after package installation by default
+cat > /etc/apt/apt.conf.d/02cleanup << EOF
+DPkg::Post-Invoke {"rm -rf /usr/share/man || true";};
+DPkg::Post-Invoke {"rm -rf /usr/share/help || true";};
+DPkg::Post-Invoke {"rm -rf /usr/share/doc || true";};
+DPkg::Post-Invoke {"rm -rf /usr/share/info || true";};
+EOF
